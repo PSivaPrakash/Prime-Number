9ba015beb98ffeb27e63f6fe06ba788154e720c8
PSivaPrakash <56464550+PSivaPrakash@users.noreply.github.com>
2020-06-18 17:03:50 +0530

    first commit

---
+++ PrimeNumbers/src/com/java/prime/PrimeNumber.java
@@ -0,0 +1,35 @@
+package com.java.prime;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		 boolean isPrime = true;
+		        int[] intArray =new int[100];
+		 
+		        int j=1;
+		        intArray[0]=2;
+		        for (int i = 3; j < 100; i++) {
+		            isPrime = prime(i);
+		            if (isPrime) {
+		            	intArray[j]=i;
+		               j++;
+		            			}
+		        		}
+		        for(int i=0;i<intArray.length;i++)
+		        	System.out.println(intArray[i]);
+		        
+		    }
+		    public static boolean prime(int no) {
+		        int rem;
+		        for (int i = 2; i <= no / 2; i++) {
+		            rem = no % i;
+		            if (rem == 0) {
+		                return false;
+		            }
+		        	}
+		        	return true;
+		 
+		    }
+		 
+		}
+
+
